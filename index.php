3720f4269e1a04cd4c0ad6c234e70bb40d4c07ab
YoanMorel <yoan.morel@live.fr>
2019-06-05 16:06:15 +0200

    lol

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+//$doc = new DOMDocument();
+//$doc->load('source.xml');
+//$htmlElements = $doc->getElementsByTagName("content");
+//foreach ($htmlElements as $htmlElement) {
+//    foreach($htmlElement->childNodes as $child) {
+//        if ($child->nodeType == XML_CDATA_SECTION_NODE) {
+//            echo $child->textContent;
+//        }
+//    }
+//}
+
+$xml = simplexml_load_file('source.xml');
+
+if($_GET):
+    
+    array_map('htmlspecialchars', $_GET);
+    extract($_GET);
+    
+    echo $page;
+    
+    echo $xml->page[$page]->content;
+    
+else:
+    echo $xml->page[0]->content;
+    
+endif;
+
+?>
