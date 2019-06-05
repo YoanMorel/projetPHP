390b4aa0debdf422363a8edd2347d491d1869a1d
Duwelzdorian <duwelzdorian@gmail.com>
2019-06-05 16:52:57 +0200

    travail sur la navbar avec yohan

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        <link rel="stylesheet" href="style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet"> 
+        <title>Exercice8</title>
+    </head>
+    <body id="bod">
+        <!--<menu>Qui sommes nous?</menu>
+            <title>En apprendre plus sur nous - Maçonnerie Ocordo</title>
+            <content><![CDATA[<h1>Qui sommes-nous?</h1>-->
+        <h2>L’AGENCE DE TRAVAUX OCORDO</h2>
+        <h3>Présentation de notre entreprise</h3>
+        <p>Nous sommes un groupement permanent d’entreprises locales du bâtiment spécialisées dans les travaux de rénovation d’appartements et de maisons et de construction d’extensions horizontale ou verticale de maisons.</p>
+        <p>Nous travaillons exclusivement pour les particuliers.</p>
+        <p><iframe src="https://www.youtube.com/embed/Crh5lFpNK0o?feature=oembed" allowfullscreen="" width="500" height="281" frameborder="0"></iframe></p>
+        <p>Notre groupement est en mesure de vous proposer les compétences métier dans le gros oeuvre et le second oeuvre :</p><p>
+        <ul>
+            <li>Maçonnerie générale</li><li>
+                Travaux de toiture</li><li>
+                Isolation thermique et phonique</li><li>
+                Plâtrerie, cloison</li><li>
+                Aménagement et agencement de votre intérieur</li><li>
+                Electricité</li><li>
+                Plomberie, sanitaire</li><li>
+                Chauffage</li><li>
+                Peinture et décoration</li><li>
+                Menuiserie</li>
+        </ul>
+
+        <p>Nous sommes aujourd’hui 7 groupements implantés à Lille, Reims, Amiens, Arras, Strasbourg, Nantes et Bordeaux.</p><p>
+
+            Quels sont les avantages en travaillant avec notre groupement ? Simplicité, Sécurité et Prix</p><p>
+            Pourquoi la Simplicité ? Parce que vous avez un seul interlocuteur qui est disponible de la préparation au chiffrage de l’ensemble de vos travaux.</p><p>
+
+            Pourquoi la Sécurité ? Par ce que pour élaborer votre projet de travaux de rénovation ou de construction d’extension nous :</p><p>
+        <ul>   
+            <li>fixons un premier rendez-vous au cours duquel nous vous écoutons nous parlez de votre projet de travaux. À ce stade nous pouvons déjà vous conseiller.</li>
+            <li>établissons un chiffrage détaillé lot par lot.</li>
+            <li>vous remettons un dossier-devis comprenant les devis détaillés avec les dates prévisionnelles de début et de fin d’intervention,les assurances décennales et la RC professionnelle,</li>
+            <li>vous remettons un résumé pratique vous expliquant l’exécution de vos travaux.</li>
+        </ul>
+        <p>Toutes les entreprises du bâtiment de notre groupement travaillent en direct sans aucune sous traitance.</p>
+
+        <p>Tous les travaux sont couverts par la garantie de parfait achèvement conformément aux règles en vigueur .</p>
+
+        <p>Pourquoi le Prix ? Nous avons professionnalisé notre organisation et mis en place des outils de gestion et d’organisation exclusifs. C’est pourquoi nous sommes en mesure de vous faire bénéficier de prix très compétitifs et d’un excellent rapport qualité/prix.<p>
+            bénéficier de prix très compétitifs et d’un excellent rapport qualité/prix pour vos travaux de rénovation.]]></content>
+
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
