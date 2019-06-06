818c81e0788b2b0073187b179efce0a7b06fe58f
YoanMorel <yoan.morel@live.fr>
2019-06-06 11:26:37 +0200

    aftermerge

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="FR">
     <head>
         <meta charset="utf-8">
@@ -10,21 +10,24 @@
         <title>projetPHP</title>
     </head>
     <body>
-    <nav class="navbar navbar-expand-md navbar-dark navbar-custom p-0 fixed-top">
+    <nav class="navbar navbar-expand-md navbar-dark navbar-custom p-0 sticky-top">
 
       <img src="assets/img/logo-ocordo-travaux.png" class="logo"/>
       <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
       <div class="collapse navbar-collapse" id="navbarContent">
         <ul class="navbar-nav mr-auto" id="navbarLink">
 
-          <li class="nav-item"> <a class="nav-link" href="#">Acceuil</a></li>
-          <li class="nav-item"> <a class="nav-link" href="#">Qui sommes nous?</a></li>
-          <li class="nav-item"> <a class="nav-link" href="#">Avis clients</a></li>
-          <li class="nav-item"> <a class="nav-link" href="#">Contact</a></li>
+          <li class="nav-item"> <a class="nav-link" href="0.html">Acceuil</a></li>
+          <li class="nav-item"> <a class="nav-link" href="1.html">Qui sommes nous?</a></li>
+          <li class="nav-item"> <a class="nav-link" href="2.html">Avis clients</a></li>
+          <li class="nav-item"> <a class="nav-link" href="3.html">Contact</a></li>
         </ul>
       </div>
     </nav>
-       <?php "include assets/partials/page.php" ?>
+    <div class="container">
+       <?php include ('assets/partials/page.php') ?>
+    </div>
+
     <!-- fin navbar -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
