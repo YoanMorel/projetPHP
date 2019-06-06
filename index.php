d3c5e764b336ff64e80be971837c73bb987222b5
Duwelzdorian <duwelzdorian@gmail.com>
2019-06-06 10:19:26 +0200

    nav footer

--- index.php
+++ index.php
@@ -8,48 +8,52 @@
         <title>Exercice8</title>
     </head>
     <body id="bod">
-        <!--<menu>Qui sommes nous?</menu>
-            <title>En apprendre plus sur nous - Maçonnerie Ocordo</title>
-            <content><![CDATA[<h1>Qui sommes-nous?</h1>-->
-        <h2>L’AGENCE DE TRAVAUX OCORDO</h2>
-        <h3>Présentation de notre entreprise</h3>
-        <p>Nous sommes un groupement permanent d’entreprises locales du bâtiment spécialisées dans les travaux de rénovation d’appartements et de maisons et de construction d’extensions horizontale ou verticale de maisons.</p>
-        <p>Nous travaillons exclusivement pour les particuliers.</p>
-        <p><iframe src="https://www.youtube.com/embed/Crh5lFpNK0o?feature=oembed" allowfullscreen="" width="500" height="281" frameborder="0"></iframe></p>
-        <p>Notre groupement est en mesure de vous proposer les compétences métier dans le gros oeuvre et le second oeuvre :</p><p>
-        <ul>
-            <li>Maçonnerie générale</li><li>
-                Travaux de toiture</li><li>
-                Isolation thermique et phonique</li><li>
-                Plâtrerie, cloison</li><li>
-                Aménagement et agencement de votre intérieur</li><li>
-                Electricité</li><li>
-                Plomberie, sanitaire</li><li>
-                Chauffage</li><li>
-                Peinture et décoration</li><li>
-                Menuiserie</li>
+        
+        <!-- MENU -->
+        <nav class="navbar navbar-expand-md navbar-dark navbar-custom p-0 fixed-top">
+      <!--  <a class="navbar-brand" href="#">DD</a> -->
+      <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav mr-auto" id="navbarLink">
+          <li class="nav-item"> <a class="nav-link" href="#">Acceuil</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#">Qui sommes nous?</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#">Avis clients</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#">Contact</a></li>
         </ul>
+      </div>
+</nav>
 
-        <p>Nous sommes aujourd’hui 7 groupements implantés à Lille, Reims, Amiens, Arras, Strasbourg, Nantes et Bordeaux.</p><p>
+        
+       
+          <!-- Footer -->
+  <footer class="main-footer">
 
-            Quels sont les avantages en travaillant avec notre groupement ? Simplicité, Sécurité et Prix</p><p>
-            Pourquoi la Simplicité ? Parce que vous avez un seul interlocuteur qui est disponible de la préparation au chiffrage de l’ensemble de vos travaux.</p><p>
+    <!--Footer Links-->
+    <div class="container mt-5 mb-4 text-center text-md-left">
+      <div class="row mt-3">
 
-            Pourquoi la Sécurité ? Par ce que pour élaborer votre projet de travaux de rénovation ou de construction d’extension nous :</p><p>
-        <ul>   
-            <li>fixons un premier rendez-vous au cours duquel nous vous écoutons nous parlez de votre projet de travaux. À ce stade nous pouvons déjà vous conseiller.</li>
-            <li>établissons un chiffrage détaillé lot par lot.</li>
-            <li>vous remettons un dossier-devis comprenant les devis détaillés avec les dates prévisionnelles de début et de fin d’intervention,les assurances décennales et la RC professionnelle,</li>
-            <li>vous remettons un résumé pratique vous expliquant l’exécution de vos travaux.</li>
-        </ul>
-        <p>Toutes les entreprises du bâtiment de notre groupement travaillent en direct sans aucune sous traitance.</p>
-
-        <p>Tous les travaux sont couverts par la garantie de parfait achèvement conformément aux règles en vigueur .</p>
-
-        <p>Pourquoi le Prix ? Nous avons professionnalisé notre organisation et mis en place des outils de gestion et d’organisation exclusifs. C’est pourquoi nous sommes en mesure de vous faire bénéficier de prix très compétitifs et d’un excellent rapport qualité/prix.<p>
-            bénéficier de prix très compétitifs et d’un excellent rapport qualité/prix pour vos travaux de rénovation.]]></content>
+        <!--First column-->
+        <div class="col-md-3 col-lg-5 mx-auto mb-4">
+          <h6 class="footer-title mt-4">À propos</h6>
+          <hr class="footerHR mb-4 mt-0 d-inline-block mx-auto" />
+          <p class="pFooter">Ocordo est une agence spécialisée dans l’externalisation du service commercial d’entreprises locales du bâtiment spécialisées dans les travaux de rénovation et de construction d’extensions. Ocordo n’assure pas la maîtrise d’oeuvre de vos travaux. Nous avons l’expérience commerciale de plus de 3.000 projets de rénovations de maisons, rénovations d’appartements, rénovations énergétiques et de constructions d’extensions de maisons.</p>
+        </div>
 
 
+
+
+      </div>
+    </div>
+
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 bg-dark">© 2019 Copyright : Yoan, Dorian & Yoan
+    </div>
+
+  </footer>
+
+        
+        
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
