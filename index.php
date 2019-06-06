e1c281f04ee8fa82f70df1ee13c272178122ff7f
Yomab <mabire.yoan@gmail.com>
2019-06-06 10:32:47 +0200

    mise a jour

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="assets/css/style.css">
         <title>projetPHP</title>
     </head>
     <body>
     <nav class="navbar navbar-expand-md navbar-dark navbar-custom p-0 fixed-top">
        
-      <img src="img/logo-ocordo-travaux.png" class="logo"/>
+      <img src="assets/img/logo-ocordo-travaux.png" class="logo"/>
       <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
       <div class="collapse navbar-collapse" id="navbarContent">
         <ul class="navbar-nav mr-auto" id="navbarLink">
@@ -24,8 +24,8 @@
         </ul>
       </div>
     </nav>
-    
-
+       <?php "include assets/partials/page.php" ?>
+    <!-- fin navbar -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
